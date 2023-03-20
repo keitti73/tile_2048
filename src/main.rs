3575dc66a8edcc98466d92dd6f8b28002f7b4ce5
keitti73 <keitti73@gmail.com>
2023-03-21 01:52:48 +0900

    add number

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use rand::Rng;
 
-#[derive(Copy,Clone)]
+#[derive(Copy,Clone,PartialEq)]
 struct Tile{
     value : usize
 }
 
-#[derive(Copy,Clone)]
+#[derive(Copy,Clone,PartialEq)]
 enum Cell {
     Tile(Tile),
     None
@@ -19,38 +20,52 @@ struct Game{
 }
 impl Game{
     fn new()-> Self{
-        let mut bord =[[Cell::None;4];4];
+        let bord =[[Cell::None;4];4];
         let bord=Bord{
             bord : bord
         };
         
-        let game = Game{
+        let mut game = Game{
             bord:bord
         };
+
+        game.add_number();
+        game.add_number();
+
         game
     }
 
     fn display(&self){
-        println!("+-------+");
+        println!("+---+---+---+---+");
         for x in 0..4{
             print!("|");
             for y in 0..4{
                 match self.bord.bord[x][y]{
-                    Cell::Tile(Tile { value })=> print!("{}|",value),
-                    Cell::None => print!(" |")
-
+                    Cell::Tile(Tile { value })=> print!(" {} |",value),
+                    Cell::None => print!("   |")
                 };
             }
             println!("");
+            println!("+---+---+---+---+");
         };
-        println!("+-------+");
     }
 
     fn add_number(& mut self){
+        loop{
+            let rng = (rand::thread_rng().gen_range(1..4),rand::thread_rng().gen_range(1..4));
+
+            if self.bord.bord[rng.0][rng.1] == Cell::None{
+                self.bord.bord[rng.0][rng.1] = Cell::Tile(Tile { value: 2 });
+                break;
+            }
+            else{
+                continue
+            }
+        }
     }
 }
 
 fn main(){
-    let new_game = Game::new();
+    let mut new_game = Game::new();
     new_game.display()
 }
